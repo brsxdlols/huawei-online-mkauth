ca5a8597a08661b38818ebf9eae8d37da6e6088c
Bruno Fontes <brunoxdlols24@gmail.com>
2026-07-29 22:08:33 -0300

    publish-config.example.php

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'nas_ip' => '10.255.255.200',
+    'snmp_host' => '10.255.255.200',
+    'snmp_version' => '2c',
+    'snmp_community' => 'ALTERE_AQUI',
+    'refresh_seconds' => 5,
+    'stale_radius_seconds' => 900,
+];
